637c9a665dda9cb2f835ee6588c4c6cfcc6f5ad9
Jorge Urbina <jorgearmandou@gmail.com>
2022-06-22 12:41:01 -0500

    creation de componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CitasComponent } from './components/citas/citas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CitasComponent
   ],
   imports: [
     BrowserModule,
